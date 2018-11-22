3ac897a9c03dd77977f6c14d6cdbf7161bad3177
fatalis <fatalis@fatalis.pw>
2018-11-22 12:15:50 -0600

    Simplify everything by using the PA unix socket

--- build_env.sh
+++ build_env.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-dockerip=$(ip addr | grep inet.*docker|awk '{print $2}'|awk -F/ '{print $1}')
-pulse_socket=(tcp:$dockerip:$((pax11publish || xprop -root PULSE_SERVER)|grep -Eo 'tcp:[^ ]*'|awk -F: '{print $3}'))
+pulse_socket=$((pax11publish || xprop -root PULSE_SERVER)|grep -Eo 'unix:[^ ]+'|awk -F: '{print $2}')
 
 echo "ARG_UID=$(id -u)" >  .env
 echo "ARG_GID=$(id -g)" >> .env
-echo "dockerip=$dockerip" >> .env
 echo "pulse_socket=$pulse_socket" >> .env
-
 
